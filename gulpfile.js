a35c42380c58560fbe09cd4eea0e9586cde25e6b
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2015-02-02 22:19:00 +0900

    Add gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
+var gulp = require( "gulp" );
+var jshint = require( "gulp-jshint" );
+var watch = require( "gulp-watch" );
 
 var paths = {
-  js : [ 'js/*.js' ]
+  js : [ "js/*.js" ]
 };
 
-gulp.task( 'jshint', function() {
+gulp.task( "jshint", function() {
   gulp.src(paths.js)
     .pipe( jshint() )
-    .pipe( jshint.reporter( 'default' ) );
+    .pipe( jshint.reporter( "default" ) );
 });
 
-gulp.task( 'default' , [ 'jshint' ] );
+gulp.task( "watch", function() {
+  gulp.watch( paths.js, [ "jshint" ]);
+});
+
+gulp.task( "default" , [ "watch" ] );
